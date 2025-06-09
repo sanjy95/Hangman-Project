089256442b587dd94b001cda069c2fb0e34cc4d1
Sanjay Singh <sanjy23955@gmail.com>
2025-06-09 20:33:43 +0100

    Implement Hangman class with input handling and basic guess checking

---
+++ Hangman Project/milestone_4.py
@@ -0,0 +1,35 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! '{guess}' is in the word.")
+        # (More logic to be added in the next task)
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Please enter a single letter: ")
+
+            if len(guess) != 1 or not guess.isalpha():
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
+
+# Test run for now
+if __name__ == "__main__":
+    words = ["mango", "banana", "grape"]
+    game = Hangman(words)
+    game.ask_for_input()
